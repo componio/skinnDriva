34d03a62cb6328705d0f9225a7dec89af043615d
Robert Diawara <robert@diawara.com>
2014-06-15 11:18:23 +0200

    Test Commit

--- opencms-commons-git/src/com/diawara/commons/taglib/CommonTagBase.java
+++ opencms-commons-git/src/com/diawara/commons/taglib/CommonTagBase.java
@@ -16,7 +16,7 @@
  *  GNU Lesser General Public License for more details.
  * 
  *  You should have received a copy of the GNU Lesser General Public License
- *  along with OpenCms Theme Engine.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with OpenCms Theme Engine.  If not, see <http://www.gnu.org/licenses/>. 
  */
 package com.diawara.commons.taglib;
 
